45d7ce898e01a25af85422ce0fdf0145aa42ae7f
Syncx <aruldevarackal1@gmail.com>
2021-08-03 08:44:51 +0530

    OK added new timetable command hehe

--- src/commands/getTime.js
+++ src/commands/getTime.js
@@ -2,7 +2,6 @@ const calcTime = require("../timeIST")
 const Classes = require('../classes')
 module.exports = {
     name: 'time',
-    aliases: ['t'],
     execute(client, message, args, _this) {
       const time = calcTime();
       message.channel.send(Classes.formatTimeHM(time));
---
+++ src/commands/timetable.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+    name: 'timetable',
+    aliases: ['t'],
+    execute(client, message, args) {
+      message.channel.send("https://media.discordapp.net/attachments/816589800542175283/871601860908306462/10ig.png");
+    },
+  };
